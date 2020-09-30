ed43d158c96aaa01afbfc455e72d8b8ecda3811b
Yuming Cheung <ymcheung.tw@gmail.com>
2020-09-30 12:34:28 +0800

    Try artist page

--- pages/artists/[slug].js
+++ pages/artists/[slug].js
@@ -1,12 +1,24 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import renderToString from 'next-mdx-remote/render-to-string';
+
+import styled, { createGlobalStyle } from 'styled-components';
+import Seo from '../../components/Seo';
+
+const root = process.cwd()
 
 export default function ArtistTemplate({ mdxSource, frontMatter }) {
   return(
-
+    <>
+      {frontMatter.fullname}
+    </>
   )
 }
-
-
 
 export async function getStaticPaths() {
   return {
@@ -18,7 +30,7 @@ export async function getStaticPaths() {
 }
 export async function getStaticProps({ params }) {
   const source = fs.readFileSync(
-    path.join(root, 'songs', `${params.slug}.mdx`),
+    path.join(root, 'artists', `${params.slug}.mdx`),
     'utf8'
   )
   const { data, content } = matter(source);
