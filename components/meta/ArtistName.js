d63f417f9e29bb53e68703735a3681854b10a3e4
Yuming Cheung <ymcheung.tw@gmail.com>
2020-11-08 08:42:24 +0800

    Add font-face for artist names

--- components/meta/ArtistName.js
+++ components/meta/ArtistName.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 import { midnight } from '../../styles/color';
+import { familyDefault } from '../../styles/font';
 
 const ArtistName = styled.h2`
   display: inline-block;
   margin: 0 0 6px;
   color: hsl(${midnight.h}, ${midnight.s}%, ${midnight.l.vii}%);
+  font-family: ${familyDefault};
   font-weight: 400;
 `;
 
